1f60566bb8c4371e4fb129b17aadd530c8e33b4b
qingfusheng <2564526674@qq.com>
2020-08-23 22:58:35 +0800

    2020/8/23 my work

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
+#include <unistd.h>
 #include <iostream>
 #include <array>
 #include <cstdio>
 #include <fstream>
 #include <string>
-#include "include/cpr/cpr.h"
+#include "cpr/cpr.h"
+void Register();
 int main(int argc, char** argv) {
+	while(1){
+		Register();
+		sleep(1);
+	}
+}
+void Register()
+{
     cpr::Url url{"http://47.111.82.206:8080/VIID/System/Register"};
     cpr::Response response = cpr::Post(url, cpr::Payload{{"x", "5"}});
-    std::cout<<response.text;
-    std::cout<<response.url;
-    std::cout<<response.header["content-type"];
-    std::cout<<response.status_code;
+    std::cout<<response.text<<std::endl;
+    std::cout<<response.url<<std::endl;
+    std::cout<<response.header["content-type"]<<std::endl;
+    std::cout<<response.status_code<<std::endl;
     if(response.error.code==cpr::ErrorCode::OK)
-        std::cout<<"OK";
+        std::cout<<"OK"<<std::endl;
     else
-        std::cout<<"ERROR";
+        std::cout<<"ERROR"<<std::endl;
 }
