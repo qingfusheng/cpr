e438c5f43ece9b1dcf884dd6ebb4f3a19d2729f7
qingfusheng <61900087+qingfusheng@users.noreply.github.com>
2020-08-26 11:03:17 +0800

    modefiled main.cpp

--- main.cpp
+++ main.cpp
@@ -8,13 +8,132 @@
 void Register();
 int main(int argc, char** argv) {
 	while(1){
-		Register();
+		Register1();
+		sleep(1);
+		Register2();
+		sleep(1);
+		Register3();
+		sleep(1);
+		Register4();
+		sleep(1);
+		Register5();
+		sleep(1);
+		Register6();
+		sleep(1);
+		Register7();
+		sleep(1);
+		Register8();
 		sleep(1);
 	}
 }
-void Register()
+void Register1()
 {
     cpr::Url url{"http://47.111.82.206:8088/VIID/System/Register"};
+    cpr::Response response = cpr::Post(url, 
+                             cpr::Payload{{"x", "5"}},
+                             cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
+    std::cout<<response.text<<std::endl;
+    std::cout<<response.url<<std::endl;
+    std::cout<<response.header["content-type"]<<std::endl;
+    std::cout<<response.status_code<<std::endl;
+    if(response.error.code==cpr::ErrorCode::OK)
+        std::cout<<"OK"<<std::endl;
+    else
+        std::cout<<"ERROR"<<std::endl;
+}
+void Register2()
+{
+    cpr::Url url{"http://47.111.82.206:8088/VIID/System/UnRegister"};
+    cpr::Response response = cpr::Post(url, 
+                             cpr::Payload{{"x", "5"}},
+                             cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
+    std::cout<<response.text<<std::endl;
+    std::cout<<response.url<<std::endl;
+    std::cout<<response.header["content-type"]<<std::endl;
+    std::cout<<response.status_code<<std::endl;
+    if(response.error.code==cpr::ErrorCode::OK)
+        std::cout<<"OK"<<std::endl;
+    else
+        std::cout<<"ERROR"<<std::endl;
+}
+void Register3()
+{
+    cpr::Url url{"http://47.111.82.206:8088/VIID/System/Keepalive"};
+    cpr::Response response = cpr::Post(url, 
+                             cpr::Payload{{"x", "5"}},
+                             cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
+    std::cout<<response.text<<std::endl;
+    std::cout<<response.url<<std::endl;
+    std::cout<<response.header["content-type"]<<std::endl;
+    std::cout<<response.status_code<<std::endl;
+    if(response.error.code==cpr::ErrorCode::OK)
+        std::cout<<"OK"<<std::endl;
+    else
+        std::cout<<"ERROR"<<std::endl;
+}
+void Register4()
+{
+    cpr::Url url{"http://47.111.82.206:8088/VIID/Faces"};
+    cpr::Response response = cpr::Post(url, 
+                             cpr::Payload{{"x", "5"}},
+                             cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
+    std::cout<<response.text<<std::endl;
+    std::cout<<response.url<<std::endl;
+    std::cout<<response.header["content-type"]<<std::endl;
+    std::cout<<response.status_code<<std::endl;
+    if(response.error.code==cpr::ErrorCode::OK)
+        std::cout<<"OK"<<std::endl;
+    else
+        std::cout<<"ERROR"<<std::endl;
+}
+void Register5()
+{
+    cpr::Url url{"http://47.111.82.206:8088/VIID/Persons"};
+    cpr::Response response = cpr::Post(url, 
+                             cpr::Payload{{"x", "5"}},
+                             cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
+    std::cout<<response.text<<std::endl;
+    std::cout<<response.url<<std::endl;
+    std::cout<<response.header["content-type"]<<std::endl;
+    std::cout<<response.status_code<<std::endl;
+    if(response.error.code==cpr::ErrorCode::OK)
+        std::cout<<"OK"<<std::endl;
+    else
+        std::cout<<"ERROR"<<std::endl;
+}
+void Register6()
+{
+    cpr::Url url{"http://47.111.82.206:8088/VIID/MotorVehicles"};
+    cpr::Response response = cpr::Post(url, 
+                             cpr::Payload{{"x", "5"}},
+                             cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
+    std::cout<<response.text<<std::endl;
+    std::cout<<response.url<<std::endl;
+    std::cout<<response.header["content-type"]<<std::endl;
+    std::cout<<response.status_code<<std::endl;
+    if(response.error.code==cpr::ErrorCode::OK)
+        std::cout<<"OK"<<std::endl;
+    else
+        std::cout<<"ERROR"<<std::endl;
+}
+void Register7()
+{
+    cpr::Url url{"http://%s/VIID/ImageServer?DeviceID=%s"};
+    cpr::Response response = cpr::Post(url, 
+                             cpr::Payload{{"x", "5"}},
+                             cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
+    std::cout<<response.text<<std::endl;
+    std::cout<<response.url<<std::endl;
+    std::cout<<response.header["content-type"]<<std::endl;
+    std::cout<<response.status_code<<std::endl;
+    if(response.error.code==cpr::ErrorCode::OK)
+        std::cout<<"OK"<<std::endl;
+    else
+        std::cout<<"ERROR"<<std::endl;
+}
+void Register8()
+{
+    cpr::Url url{"http://%s:%d/VIID/Images"};
     cpr::Response response = cpr::Post(url, 
                              cpr::Payload{{"x", "5"}},
                              cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
