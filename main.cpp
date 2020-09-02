3cdd71a86c34bf19c1a43651b4db2b8278ee3f1e
qingfusheng <2564526674@qq.com>
2020-09-02 15:33:26 +0800

    main

--- main.cpp
+++ main.cpp
@@ -15,14 +15,14 @@ void Register7();
 void Register8();
 int main(int argc, char** argv) {
 	while(1){
-		Register1();sleep(1);
-		Register2();sleep(1);
-		Register3();sleep(1);
-		Register4();sleep(1);
+		Register1();sleep(20);
+		Register2();sleep(20);
+		Register3();sleep(20);
+		Register4();sleep(20);
 		/*Register5();sleep(1);*/
-		Register6();sleep(1);
-		Register7();sleep(1);
-		Register8();sleep(1);
+		Register6();sleep(20);
+		Register7();sleep(20);
+		Register8();sleep(20);
 	}
 }
 void Register1()
@@ -30,7 +30,7 @@ void Register1()
     cpr::Url url{"http://47.111.82.206:8088/VIID/System/Register"};
     cpr::Response response = cpr::Post(url, 
                             /* cpr::Payload{{"x", "5"}},*/
-                             cpr::Body{"{"RegisterObject":{\"DeviceID\":\"31000000001310910561\",\"ProtocolVersion\":\"2.0\"}}"},
+                             cpr::Body{"{\"RegisterObject\":{\"DeviceID\":\"31000000001310910561\",\"ProtocolVersion\":\"2.0\"}}"},
                              cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
     std::cout<<response.text<<std::endl;
     std::cout<<response.url<<std::endl;
@@ -61,7 +61,7 @@ void Register3()
     cpr::Url url{"http://47.111.82.206:8088/VIID/System/Keepalive"};
     cpr::Response response = cpr::Post(url, 
                             /* cpr::Payload{{"x", "5"}},*/
-                             cpr::Body{"{\"KeepaliveObject\":\{\"DeviceID\":\"31000000001310910561\"}}"},
+                             cpr::Body{"{\"KeepaliveObject\":{\"DeviceID\":\"31000000001310910561\"}}"},
                              cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
     std::cout<<response.text<<std::endl;
     std::cout<<response.url<<std::endl;
@@ -137,7 +137,7 @@ void Register8()
     cpr::Url url{"http://47.111.82.206:8088/VIID/Images"};
     cpr::Response response = cpr::Post(url, 
                             /* cpr::Payload{{"x", "5"}},*/
-                             cpr::Body{"{\"RegisterObject\":{\"DeviceID\":\"31000000001310910561\",\"ProtocolVersion\":\"2.0\"}}"}
+                             cpr::Body{"{\"RegisterObject\":{\"DeviceID\":\"31000000001310910561\",\"ProtocolVersion\":\"2.0\"}}"},
                              cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
     std::cout<<response.text<<std::endl;
     std::cout<<response.url<<std::endl;
