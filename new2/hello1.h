82213ee1942a280dabc055a21832afa6832966df
qingfusheng <61900087+qingfusheng@users.noreply.github.com>
2020-10-11 13:35:28 +0800

    HELLO

--- new2/hello1.h
+++ new2/hello1.h
@@ -11,7 +11,7 @@ public:
     bool Unregister(); //向服务器注销，返回对错
     bool Keepalive();  //向服务器保活，返回对错
 private:
-    cpr::Session session;
+    cpr::Session session;//使用session对象包装以达到网络连接复用的目的
 };
 GAT1400Server::GAT1400Server(){ 
 };
@@ -20,21 +20,21 @@ GAT1400Server::GAT1400Server(std::string &url_){
     session.SetUrl(url);
     session.SetOption(cpr::Header{{"Content-Type","application/json;charset=utf-8"}});
 }
-GAT1400Server::~GAT1400Server(){
+GAT1400Server::~GAT1400Server(){ //析构
     delete session;
 
 };
 GAT1400Server::Register(std::string deviceid){
-    json j;
+    json j; //定义一个json串
 	j["RegisterObject"]={"DeviceID",deviceid};
 	j["ProtocolVersion"]="2.0";
-    std::string j_string = j.dump();
+    std::string j_string = j.dump();//将json串转换为字符串
     session.SetOption(cpr::Body{j_string});
     cpr::Response response = session.Post();
-    std::cout<<response.text<<std::endl;
-    std::cout<<response.url<<std::endl;
-    std::cout<<response.header["content-type"]<<std::endl;
-    std::cout<<response.status_code<<std::endl;
+    std::cout<<response.text<<std::endl;    //expected_text
+    std::cout<<response.url<<std::endl;     //url
+    std::cout<<response.header["content-type"]<<std::endl;  //std::string{"application/json"}
+    std::cout<<response.status_code<<std::endl; //status_code
     if(response.error.code==cpr::ErrorCode::OK)
         std::cout<<"OK"<<std::endl;
     else
